08c6f6219fd9f1711d40a97bc0c75e14d9a5e027
Igor Guryan <sacujo93@yandex.ru>
2022-10-27 12:05:46 +0300

    finishing the exercise

--- Project2_HackingWithSwift/ViewController.swift
+++ Project2_HackingWithSwift/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     var countries = [String]()
     var correctAnswer = 0
     var score = 0
+    var questionSum = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,16 +35,41 @@ class ViewController: UIViewController {
         askQuestion()
     }
     
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
         countries.shuffle()
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
         correctAnswer = Int.random(in: 0...2)
-        title = countries[correctAnswer].uppercased()
+        title = countries[correctAnswer].uppercased() + " Score: \(score)"
 
     }
     @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        var message: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+            message = "Your score is \(score)"
+        } else {
+            title = "Wrong"
+            score -= 1
+            message = "That's the flag of \(countries[sender.tag].uppercased())! Your score is \(score)"
+        }
+        
+        questionSum += 1
+        
+        if questionSum == 10 {
+            let ac = UIAlertController(title: "Congratuation!", message: "Game is over! Your score is \(score)", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+            present(ac,animated: true)
+        } else {
+            
+            let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+            present(ac,animated: true)
+        }
     }
     
 
